7c7e9598360da0a6b68e28ea90fed4e93b343c47
Eva Singh <eva.singh@hotmail.com>
2020-09-08 21:28:45 +0200

    Change the name of the file and add function that limits card selection.

---
+++ JS/cards.js
@@ -0,0 +1,66 @@
+fetch("../JSON/characters.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        displayCharacters(json);
+    });
+
+function displayCharacters(characters) {
+    
+    let newHTML = "";
+    
+    characters.forEach(function(character) {
+        
+        newHTML += `<div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src=" ${character.image} " alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">${character.name}</h5>
+                                <p class="card-text">${character.description}</p>
+                                <label class="btn btn-primary btn-block ">
+                                <input type="checkbox" autocomplete="off"> Pick me!
+                                </label>
+                            </div>
+                         </div>
+                    </div>`;  
+        
+        
+    });
+       
+    const characterSection = document.querySelector('.characterCards');
+    characterSection.innerHTML = newHTML;
+    
+    checkBoxLimit();
+}
+
+function checkBoxLimit() {
+    
+	const checkBoxButtons = document.getElementById('checkboxbuttons').getElementsByTagName("input");
+	let limit = 2;
+	
+	for (let i = 0; i < checkBoxButtons.length; i++) {
+		checkBoxButtons[i].onclick = function() {
+        var checkedCount = 0;
+            
+			for (let i = 0; i < checkBoxButtons.length; i++) {
+				checkedCount += (checkBoxButtons[i].checked) ? 1 : 0;
+			}
+			
+			if (checkedCount > limit) {
+				alert("Eppeppepp! Only " + limit + "!");
+				this.checked = false;
+			}
+			
+			if (checkedCount >= limit) {
+                document.querySelector(".submit").classList.remove("disabled");
+			}
+            
+            else {
+                 document.querySelector(".submit").classList.add("disabled"); 
+            }
+		}
+	}
+}
+
+checkBoxLimit()
