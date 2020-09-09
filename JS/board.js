c46e2f8b60688cce70cbbdc6b07b1f716782c951
Eva Singh <eva.singh@hotmail.com>
2020-09-10 00:25:58 +0200

    Draw images for the players and the throne on tilemap.

--- JS/board.js
+++ JS/board.js
@@ -1,3 +1,83 @@
+//The game board
+
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+const character1 = new Image();
+character1.addEventListener('load', eventImageLoaded, false);
+
+const character2 = new Image();
+character2.addEventListener('load', eventImageLoaded, false);
+
+const throne = new Image();
+throne.addEventListener('load', eventImageLoaded, false);
+
+character1.src = "images/sansa-token.png";
+character2.src = "images/daenerys-token.png";
+throne.src = "images/throne.png";
+    
+const tileSize = 40;
+
+const tileColors = {
+    0: {color:'#B7D5E5'},
+    1: {color:'#ffffff'},
+    2: {color:'#1C5A18'},
+    3: {color:'#59321C'}
+}
+
+const map = {
+    cols: 30,
+    rows: 10,
+    width: 32 * tileSize,
+    height: 12 * tileSize,
+    tiles: [
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,
+        0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,1,1,1,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+        3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3  
+    ]
+ };
+
+function eventImageLoaded() {
+    drawTiles()
+}
+
+function drawTiles() {
+    var mapIndex = 0;
+
+    for (var top = 0; top < map.height; top += tileSize){
+
+       for(var left =0; left < map.width; left += tileSize) {
+
+           var tileValue = map.tiles[mapIndex];
+           var tile = tileColors[tileValue];
+           let pos = 0;
+
+           ctx.fillStyle = tile.color;
+           ctx.fillRect(left, top, tileSize, tileSize);
+           mapIndex ++;
+           
+           ctx.drawImage(throne, 1152, 190, 128, 250 );
+           ctx.drawImage(character1, pos+40, 280, 64, 120 );
+           ctx.drawImage(character2, pos, 320, 64, 120 );
+       }  
+    }
+}
+
+ctx.canvas.width = map.width;
+ctx.canvas.height = map.height;
+
+
+//Die roll
+
 document.querySelector(".panel1").classList.remove("active");
 document.querySelector(".panel2").classList.remove("active");
 
@@ -13,6 +93,7 @@ function rollDie() {
             document.querySelector(".dice").src = "../Images/d" + randomNumber + ".png";
             
             //Move character corresponding steps
+        
             
             //Check that player didn't roll a 6 and switch players if not
             if(randomNumber != 6) {
@@ -28,62 +109,3 @@ function switchPlayer() {
 }
 
 rollDie();
-
-const canvas = document.getElementById('board');
-const ctx = canvas.getContext('2d');
-    
-const tileSize = 40;
-
-const tileColors = {
-    0: {color:'#8DCBF2'},
-    1: {color:'#ffffff'},
-    2: {color:'#038C33'},
-    3: {color:'#59321C'}
-}
-
-const map = {
-    cols: 30,
-    rows: 10,
-    width: 30 * tileSize,
-    height: 15 * tileSize,
-    tiles: [
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,1,1,1,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-        3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
-        3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3
-    ]
- };
-
-function drawTiles() {
-    var mapIndex = 0;
-
-    for (var top = 0; top < map.height; top += tileSize){
-
-       for(var left =0; left < map.width; left += tileSize) {
-
-           var tileValue = map.tiles[mapIndex];
-           var tile = tileColors[tileValue];
-
-           ctx.fillStyle = tile.color;
-           ctx.fillRect(left, top, tileSize, tileSize);
-           mapIndex ++;
-
-       }  
-    }
-}
-
-ctx.canvas.width = map.width;
-ctx.canvas.height = map.height;
-
-drawTiles();
