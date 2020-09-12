6b97bf083743ec5fb6187237937737604007fe0d
Eva Singh <eva.singh@hotmail.com>
2020-09-12 22:19:35 +0200

    Display winning token on page and polish animated banner.

--- JS/finale.js
+++ JS/finale.js
@@ -1,14 +1,39 @@
-var ctx = document.getElementById("banner").getContext("2d");
-var path = 0;
+fetch("../JSON/characters.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (characters) {
+        const winnerId = window.location.search.replace('?', '');
+        const winner = characters.find(c => c.id === +winnerId);
+
+        document.getElementById("winner").src = winner.token;
+    });
+    
+//Canvas animation
+
+const ctx = document.getElementById("banner").getContext("2d");
+let path = 0;
+let movingRight = true;
 
 function drawText(path) {
-    ctx.font = "bold 64px Verdana";
+    ctx.font = "bold 40px Sofia-Pro-Soft";
     ctx.fillStyle = "#52ce90";
-    ctx.fillText("Congratulations! You won!", path, 100);
+    ctx.fillText("Winner!", path, 100);
 }
 
 setInterval(function(){
-    ctx.clearRect(0,0,1280,200);
-    drawText(path%1280);
-    path++;
+    ctx.clearRect(0,0,500,150);
+    drawText(path%500);
+    
+   if (movingRight) {
+            path++;
+            if (path === 350) {
+                movingRight = false;
+            }
+        }else {
+            path--;
+            if (path === 0) {
+                movingRight = true;
+            }
+        } 
  }, 10);
